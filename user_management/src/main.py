17d925e9bec912f8d6b136561692d49598b41aa2
Aniket kadukar <aniketdkadukar@gmail.com>
2025-04-21 00:08:03 +0530

    Create user_management service

---
+++ user_management/src/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from .database.core import engine, Base
+from .entities.user import User
+from .api import register_routes
+from .logging import configure_logging, LogLevels
+
+configure_logging(LogLevels.info)
+
+app = FastAPI()
+
+""" Only uncomment below to create new tables,
+otherwise the tests will fail if not connected
+"""
+# Base.metadata.create_all(bind=engine)
+
+register_routes(app)
